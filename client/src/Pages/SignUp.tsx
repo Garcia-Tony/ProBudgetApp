36ff44d4111e20d0b577d4bbd785c3b8a52acbf0
Anthony Garcia <anthony13241@gmail.com>
2025-04-03 02:47:39 +0000

    Create functionality for password bar

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -1,18 +1,35 @@
 export function RegistrationForm() {
   return (
-    <div className="mb-1 md:flex justify-center">
-      <label className="mb-1 block">
-        <span className="md:text-3xl underline text-2xl text-white">
-          Username
-        </span>
-        <input
-          required
-          name="username"
-          placeholder="Username"
-          type="text"
-          className="text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
-        />
-      </label>
+    <div className="flex flex-col items-center justify-center h-screen bg-[#13878B]">
+      <div className="mb-1 md:flex justify-center">
+        <label className="mb-1 block">
+          <span className="md:text-3xl underline text-2xl text-white">
+            Username
+          </span>
+          <input
+            required
+            name="username"
+            placeholder="Username"
+            type="text"
+            className="text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
+          />
+        </label>
+      </div>
+      <br></br>
+      <div className="mb-1 md:flex justify-center">
+        <label className="mb-1 block">
+          <span className="md:text-3xl underline text-2xl text-white">
+            Password
+          </span>
+          <input
+            required
+            name="password"
+            placeholder="Password"
+            type="password"
+            className=" text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
+          />
+        </label>
+      </div>
     </div>
   );
 }
