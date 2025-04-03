8880b560bea2d2dc3692d7ec30a7c479e8ec4971
Anthony Garcia <anthony13241@gmail.com>
2025-04-03 04:07:21 +0000

    Create functionality for password bar in login page

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -15,6 +15,21 @@ export function SignInForm() {
             className="text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
           />
         </label>
+        <br></br>
+        <div className="mb-1 md:flex justify-center">
+          <label className="mb-1 block">
+            <span className="md:text-3xl underline text-2xl text-white">
+              Password
+            </span>
+            <input
+              required
+              name="password"
+              placeholder="Password"
+              type="password"
+              className=" text-2xl block border border-gray-600 rounded p-2 h-12 w-full md:w-[500px] lg:w-[600px] mb-2 mt-2"
+            />
+          </label>
+        </div>
       </div>
     </div>
   );
