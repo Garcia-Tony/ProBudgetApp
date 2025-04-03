1a51d9756e87b2d80459523621e41f23ad8ac30c
Anthony Garcia <anthony13241@gmail.com>
2025-04-03 04:42:01 +0000

    Create the menu bar with hover transition and popup functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { AuthPage } from './Pages/SwitchPage';
 import { UserProvider } from './components/UserContext';
 import { RegistrationForm } from './Pages/SignUp';
-
+import { Home } from './Pages/Home';
 export function App() {
   return (
     <UserProvider>
@@ -10,6 +10,7 @@ export function App() {
         <Route index element={<RegistrationForm />} />
         <Route path="/sign-up" element={<Navigate to="/" />} />
         <Route path="/log-in" element={<AuthPage mode="log-in" />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </UserProvider>
   );
---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export function Home() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  return (
+    <div className="relative flex-grow flex-1 pl-2 px-4">
+      <div className="flex items-center space-x-4">
+        <button
+          className="rounded py-2 px-1.5 bg-white hover:bg-gray-200 transition mt-6"
+          onClick={toggleMenu}>
+          <svg
+            className="md:w-[50px] md:h-[50px] w-8 h-8 text-[#01898B]"
+            viewBox="0 0 448 512">
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div
+          className={`absolute top-0 left-0 h-screen w-64 bg-white shadow-md border transition-all transform ease-in-out
+  ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+  shadow-lg shadow-black md:w-1/2`}></div>
+      )}
+    </div>
+  );
+}
