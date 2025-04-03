a74b4cf6a55bd19fceda074ced676795f7736258
Anthony Garcia <anthony13241@gmail.com>
2025-04-03 04:48:15 +0000

    Create functionality for log out button with hover transition on popup

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -40,9 +40,14 @@ export function Home() {
               />
             </svg>
           </button>
+
           <h2 className="text-4xl ml-3 text-[#01898B] font-bold mt-8 md:text-5xl md:ml-[25px]">
             Menu
           </h2>
+
+          <button className="md:text-4xl md:px-28 md:ml-[25px] text-2xl block text-center border border-[#01898B] rounded-full py-1 px-[55px] ml-3 mt-10 bg-[#01898B] text-white  hover:bg-[#016B6D] transition">
+            Log Out
+          </button>
         </div>
       )}
     </div>
